7d57129c0a8b5c0ab3d360c5f67b2875f7075e6b
jakubrozumek <jakub.rozumek@hotmail.com>
2024-01-30 18:39:54 +0100

    Přidejte soubory projektu.

---
+++ Kalkulačka/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kalkulačka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
