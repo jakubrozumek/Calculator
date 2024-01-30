0b05850e22836de095de75e71e392ad92046b6cc
jakubrozumek <jakub.rozumek@hotmail.com>
2024-01-30 19:09:46 +0100

    First changes

--- Kalkulačka/Program.cs
+++ Kalkulačka/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Console.WriteLine("Enter number to be added:");
+            string s = Console.ReadLine();
+
+            Console.WriteLine("Enter number to be added:");
+            string s1 = Console.ReadLine();
+
+            int sum = int.Parse(s) + int.Parse(s1);
+
+            string ll = (sum > 20) ? "more": "less";
+            
+            Console.WriteLine("Result is " + ll + " than " + 20.);
         }
     }
 }
