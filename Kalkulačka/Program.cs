be64ceeabfa9e99cab085e6403f47dfeb18dd65b
jakubrozumek <jakub.rozumek@hotmail.com>
2024-02-03 23:22:09 +0100

    switch used

--- Kalkulačka/Program.cs
+++ Kalkulačka/Program.cs
@@ -5,18 +5,45 @@
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Enter number to be added:");
+            Console.WriteLine("Enter first number:");
             string s = Console.ReadLine();
 
-            Console.WriteLine("Enter number to be added:");
+            Console.WriteLine("Enter second number:");
             string s1 = Console.ReadLine();
 
-            int sum = int.Parse(s) + int.Parse(s1);
+            //int sum = int.Parse(s) + int.Parse(s1);
 
             /*string ll = (sum > 20) ? "more": "less";
             
             Console.WriteLine("Result is " + ll + " than " + 20 + ".");*/
-            Console.WriteLine("Result: " + sum);
+
+            Console.WriteLine("Enter operator:");
+            string wOperator = Console.ReadLine();
+            float wResult;
+            switch(wOperator)
+            {
+                case "+":
+                    wResult = float.Parse(s) + float.Parse(s1);
+                break;
+
+                case "-":
+                    wResult = float.Parse(s) - float.Parse(s1); 
+                break;
+
+                case "*":
+                    wResult = float.Parse(s) * float.Parse(s1);
+                break;
+
+                case "/":
+                    wResult = float.Parse(s1) / float.Parse(s1);
+                break;
+
+                default:
+                    wResult = 0;
+                break;
+
+            }
+            Console.WriteLine("Result: " + wResult);
         }
     }
 }
