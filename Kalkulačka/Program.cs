f434bd2c3e029b4e5bd14c9c60b7ed0984620b28
jakubrozumek <jakub.rozumek@hotmail.com>
2024-02-04 18:16:29 +0100

    Switch modified

--- Kalkulačka/Program.cs
+++ Kalkulačka/Program.cs
@@ -6,11 +6,13 @@
         {
             
             Console.WriteLine("Enter first number:");
-            string s = Console.ReadLine();
+            double nr1 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
 
             Console.WriteLine("Enter second number:");
-            string s1 = Console.ReadLine();
-
+            double nr2 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+                
             //int sum = int.Parse(s) + int.Parse(s1);
 
             /*string ll = (sum > 20) ? "more": "less";
@@ -19,23 +21,27 @@
 
             Console.WriteLine("Enter operator:");
             string wOperator = Console.ReadLine();
-            float wResult;
+            double wResult;
             switch(wOperator)
             {
                 case "+":
-                    wResult = float.Parse(s) + float.Parse(s1);
+                    wResult = nr1 + nr2;
                 break;
 
                 case "-":
-                    wResult = float.Parse(s) - float.Parse(s1); 
+                    wResult = nr1 - nr2; 
                 break;
 
                 case "*":
-                    wResult = float.Parse(s) * float.Parse(s1);
+                    wResult = nr1 *  nr2;
                 break;
 
                 case "/":
-                    wResult = float.Parse(s1) / float.Parse(s1);
+                    if (nr2 != 0)
+                        wResult = nr2 / nr1;
+                    else
+                        Console.WriteLine("Error");
+                    wResult = 0;
                 break;
 
                 default:
